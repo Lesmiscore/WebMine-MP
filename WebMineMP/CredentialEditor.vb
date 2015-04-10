7c1260831a56a0682ddb2d6e12490a6d1a1451f0
nao20010128nao <nao20010128@gmail.com>
2015-04-10 23:55:12 +0900

    言い難い(言いたくない)バグの修正

--- WebMineMP/CredentialEditor.vb
+++ WebMineMP/CredentialEditor.vb
@@ -7,7 +7,7 @@ Public Class CredentialEditor
         If Not Boolean.TryParse(config("AvaliableWMMPInfo"), WMMPLoginEnabled.Checked) Then
             WMMPLoginEnabled.Checked = False
         End If
-        CredentialGroup.Enabled = Not WMMPLoginEnabled.Checked
+        CredentialGroup.Enabled = WMMPLoginEnabled.Checked
         If config.Keys.Contains("WMMP_ID") Then 'IDがあれば取得
             WMMPID.Text = dec(config("WMMP_ID"))
         End If
@@ -15,14 +15,14 @@ Public Class CredentialEditor
             WMMPPW.Text = dec(config("WMMP_PASS"))
         End If
     End Sub
-     Function dec(s As String) As String
+    Function dec(s As String) As String
         Return Encoding.UTF8.GetString(Convert.FromBase64String(s))
     End Function
     Function enc(s As String) As String
         Return Convert.ToBase64String(Encoding.UTF8.GetBytes(s))
     End Function
     Private Sub WMMPLoginEnabled_CheckedChanged(sender As Object, e As EventArgs) Handles WMMPLoginEnabled.CheckedChanged
-        CredentialGroup.Enabled = Not WMMPLoginEnabled.Checked
+        CredentialGroup.Enabled = WMMPLoginEnabled.Checked
     End Sub
     Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
         config("AvaliableWMMPInfo") = WMMPLoginEnabled.Checked
